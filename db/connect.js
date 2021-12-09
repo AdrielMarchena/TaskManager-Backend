75fe7d83ef4195ccaeecd69533409dc9aaa46d63
Adriel Marchena <42892229+AdrielMarchena@users.noreply.github.com>
2021-12-09 14:47:33 -0300

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb+srv://adriel:Astralopteco442@cluster0.zkoqt.mongodb.net/TaskManager?retryWrites=true&w=majority";
-
 const connectDB = (url) =>
 {
         return mongoose.connect(url);    
